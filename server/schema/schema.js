cf35afb2fce1d30a4e3d2dde3c553c98582df8ee
Siddhesh Kand <21078949+siddheshkand@users.noreply.github.com>
2021-10-19 01:08:27 +0530

    Add Hobby Schema

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -9,6 +9,22 @@ var usersData = [
     { id: '150', name: 'Georgina', age: 36, profession: 'Teacher' }
 ];
 
+var hobbiesData = [
+    { id: '1', title: 'Programming', description: 'Using computers to make the world a better place', userId: '150' },
+    { id: '2', title: 'Rowing', description: 'Sweat and feel better before eating donouts', userId: '211' },
+    { id: '3', title: 'Swimming', description: 'Get in the water and learn to become the water', userId: '211' },
+    { id: '4', title: 'Fencing', description: 'A hobby for fency people', userId: '13' },
+    { id: '5', title: 'Hiking', description: 'Wear hiking boots and explore the world', userId: '150' },
+];
+
+// var postsData = [
+//     {id: '1', comment: 'Building a Mind', userId: '1'},
+//     {id: '2', comment: 'GraphQL is Amazing', userId: '1'},
+//     {id: '3', comment: 'How to Change the World', userId: '19'},
+//     {id: '4', comment: 'How to Change the World', userId: '211'},
+//     {id: '5', comment: 'How to Change the World', userId: '1'}
+// ]
+
 // Create Type
 const UserType = new graphql.GraphQLObjectType({
     name: 'User',
@@ -22,6 +38,16 @@ const UserType = new graphql.GraphQLObjectType({
     }
 })
 
+const HobbyType = new graphql.GraphQLObjectType({
+    name: "Hobby",
+    description: "Hobby Description",
+    fields: () => ({
+        id: { type: graphql.GraphQLString },
+        title: { type: graphql.GraphQLString },
+        description: { type: graphql.GraphQLString },
+    })
+});
+
 // Root Query
 const RootQuery = new graphql.GraphQLObjectType({
     name: 'RootQueryType',
@@ -32,8 +58,19 @@ const RootQuery = new graphql.GraphQLObjectType({
             args: { id: { type: graphql.GraphQLString } },
             resolve(parent, args) {
                 // return data from data source
-                return _.find(usersData, { id: args.id })
+                if(args.id){
+                    return _.find(usersData, { id: args.id })
+                }else {
+                    return usersData;
+                }
             },
+        },
+        hobby: {
+            type: HobbyType,
+            args: { id: { type: graphql.GraphQLID } },
+            resolve(parent, args) {
+                return _.find(hobbiesData, { id: args.id })
+            }
         }
     }
 })
